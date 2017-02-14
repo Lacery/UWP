5ba2220e8c414d56aedcf23655d868c24dac9ef7
K2365 <K2365@TIET-327-WS-11>
2017-02-14 11:34:07 +0200

    tehtävä 4 about valmis

--- UWP/MainPage.xaml.cs
+++ UWP/MainPage.xaml.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿
+
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -22,9 +25,46 @@ namespace UWP
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        double ikkunanLeveys, ikkunanKorkeus, puunLeveys = 45;
         public MainPage()
         {
             this.InitializeComponent();
         }
+
+
+        private void laskeButton_Click(object sender, RoutedEventArgs e)
+        {
+            double vaihto = (ikkunanLeveys+puunLeveys*2) * (ikkunanKorkeus + puunLeveys*2);
+            double lasi = ikkunanLeveys * ikkunanKorkeus;
+            double karmi = ikkunanKorkeus * 2 + (ikkunanLeveys - puunLeveys) * 2;
+            ikkunanPintaAlaTextBox.Text = vaihto.ToString() + " cm^2";
+            lasinPintaAlaTextBox.Text = lasi.ToString() + " cm^2";
+            karminPiiriTextBox.Text = karmi.ToString() + " mm";
+        }
+
+        private void puunLeveysTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (!double.TryParse(puunLeveysTextBox.Text, out puunLeveys))
+            {
+
+            }
+        }
+
+        private void ikkunanLeveysTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (!double.TryParse(ikkunanLeveysTextBox.Text, out ikkunanLeveys))
+            {
+                
+            }
+
+        }
+        private void ikkunanKorkeusTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (!double.TryParse(ikkunanKorkeusTextBox.Text, out ikkunanKorkeus))
+            {
+
+            }
+        }
+
     }
 }
