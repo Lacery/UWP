bbc28434a34fdcf852dd955142c8648542f2091b
Ville Halminen <vhalminen@gmail.com>
2017-03-09 14:25:05 +0200

    Tehtava 4 edistyy

--- UWP/t4/MainPage.xaml.cs
+++ UWP/t4/MainPage.xaml.cs
@@ -26,5 +26,90 @@ namespace t4
         {
             this.InitializeComponent();
         }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            double value = double.Parse(valueTextBox.Text);
+            if (value == 0) //Jos valueTextBoxissa on 0, tyhjennetaan se >>>> HOX! Jos value on "" eli tyhja, ei toimi -> Tehtava jokin saanto
+            {
+                valueTextBox.Text = "";
+            }
+            string buttonString = (((Button)sender).Content).ToString();
+            valueTextBox.Text += buttonString;
+        }
+
+        private void buttonBack_Click(object sender, RoutedEventArgs e)
+        {
+            string value = valueTextBox.Text;
+            value = value.Remove(value.Length - 1);
+            valueTextBox.Text = value;
+        }
+
+        private void buttonOk_Click(object sender, RoutedEventArgs e)
+        {
+            Kiuas kiuas = new Kiuas();
+            double value = double.Parse(valueTextBox.Text);
+
+            if ((bool)tempCheckBox.IsChecked)
+            {
+                double tempChecked = kiuas.CheckTemp(value);
+                tempTextBox.Text = tempChecked.ToString();
+                if (tempChecked == 0)
+                {
+                    infoTextBlock.Text = "Info: Invalid value. Temperature must be 0-120";
+                }
+            }
+
+            if ((bool)humidCheckBox.IsChecked) {
+                double humidChecked = kiuas.CheckHumid(value);
+                humidTextBox.Text = humidChecked.ToString();
+                if (humidChecked == 0)
+                {
+                    infoTextBlock.Text = "Info: Invalid value. Humidity must be 0-100";
+                }
+            }
+
+
+        }
+
+        class Kiuas
+        {
+            private double temperature;
+            private double humidity;
+
+
+            public double Temperature {
+                get { return temperature; }
+                set { if (value <= 120 && value >= 0) { temperature = value; }
+                    else { temperature = 0; }
+                }
+            }
+            
+            public double Humidity {
+                get { return humidity; }
+                set {
+                    if (value <= 100 && value >= 0){
+                        humidity = value;
+                    }
+                    else { humidity = 0; }
+                }
+            }
+
+            public double CheckTemp(double temp)
+            {
+                Temperature = temp;
+                return temperature;
+            }
+
+            public double CheckHumid(double humid)
+            {
+                Humidity = humid;
+                return humidity;
+            }
+
+
+
+        }
+        
     }
 }
